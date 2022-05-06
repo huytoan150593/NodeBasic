9ddfccf00f1d5656db300f925f530688f062b674
huytoan150593 <mrtoan150593@gmail.com>
2022-05-06 22:17:52 +0900

    create basic router follow MVC design pattern

---
+++ src/route/web.js
@@ -0,0 +1,15 @@
+import express from "express";
+import homeController from "../controller/homeController"
+
+const router = express.Router();
+
+const initWebRouter = (app) => {
+    router.get('/', homeController.getHomePage)
+    router.get('/about', (req, res) => {
+        res.send("I'm Toan!!")
+    })
+
+    return app.use('/', router)
+}
+
+export default initWebRouter;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRouter from "./route/web";
 
 const app = express()
 const port = process.env.PORT || 8080;
 
 configViewEngine(app)
-app.get('/', (req, res) => {
-    res.render("index.ejs")
-})
-
-app.get('/about', (req, res) => {
-    res.send("I'm Toan!!")
-})
+initWebRouter(app)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on http://localhost:${port}`)
 })
